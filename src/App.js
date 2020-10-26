e086ae9c1b1ef4565358ac6558a29085bab68eeb
ryoung7986 <ryoung7986@gmail.com>
2020-10-26 10:46:21 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Discord Clone App</h1>
+    // <sidebar />
+    // <chat body />
+  );
+}
+
+export default App;
